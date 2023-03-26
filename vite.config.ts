8555f95c6d28a31305c68443cbc3df2ca5d02a36
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-03-26 11:43:22 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gpti-erp-project/',
+  base: '/GPTI-ERP-Project/',
   plugins: [react()],
 })
