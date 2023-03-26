288594ac5c2c11cf31bf97be3a1b1aa6de7afd23
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-03-26 10:57:32 -0300

    add dist

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className='page-container'>
-      <BrowserRouter basename="/GPTI-ERP-Project">
+      <BrowserRouter>
         <Header />
         <Rotas />
       </BrowserRouter>
