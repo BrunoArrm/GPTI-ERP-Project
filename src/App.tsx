f9aadb21f6664ec105e5f9a0248abd1dbfc2f476
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-03-26 02:02:48 -0300

    Basename no BrowserRouter foi configurado corretamente para permitir o deploy no ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className='page-container'>
-      <BrowserRouter>
+      <BrowserRouter basename="/GPTI-ERP-Project">
         <Header />
         <Rotas />
       </BrowserRouter>
